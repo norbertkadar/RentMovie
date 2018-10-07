2524af386c96a93ab3a05eaffbd53fc2c6c1041e
CodrutaH <hontaucodruta@yahoo.com>
2018-10-07 17:06:30 +0300

    mesaj

--- h1/src/main/java/ro/ubb/h1/Main.java
+++ h1/src/main/java/ro/ubb/h1/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("h1");
         System.out.println("hello");
+        System.out.println("ce faci?");
     }
 }
